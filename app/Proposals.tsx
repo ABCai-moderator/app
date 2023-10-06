32dd358d7792e7c81ef1d795b6d45206649d4392
Sandoche <sandoche@protonmail.com>
2023-10-06 21:59:00 +0300

    adding refetch interval for the frontend

--- app/Proposals.tsx
+++ app/Proposals.tsx
@@ -4,11 +4,13 @@ import Proposal from "./Proposal";
 import { Grid } from "@mantine/core";
 
 export default function Proposals() {
-  const query = useQuery("proposals", fetchProposals);
+  const query = useQuery("proposals", fetchProposals, {
+    refetchInterval: 1000,
+  });
 
   const proposals = query.data?.proposals?.map((proposal: any) => {
     return (
-      <Grid.Col span={6} key={proposal.id}>
+      <Grid.Col span={{ base: 12, md: 6 }} key={proposal.id}>
         <Proposal
           status={proposal.status}
           number={proposal.id}
