c2638949504a692cdeaa91a2f6931eb8c2bff416
Sandoche <sandoche@protonmail.com>
2023-10-06 20:24:03 +0300

    Fixing typo

--- app/api-services.ts
+++ app/api-services.ts
@@ -1,6 +1,6 @@
 export const fetchProposals = async () => {
   const response = await fetch(
-    `${process.env.NEXT_PULIC_COSMOS_REST_RPC}/cosmos/gov/v1/proposals?pagination.reverse=true`
+    `${process.env.NEXT_PUBLIC_COSMOS_REST_RPC}/cosmos/gov/v1/proposals?pagination.reverse=true`
   );
   const proposals = await response.json();
   return proposals;
